d2f2a383c319362b23a224241e21e902239b4475
Anthony Generoso Pascale <anthon1993@hotmail.com>
2022-05-17 09:57:48 +0100

    Update randomNumber.kt
    I forgot to comment out the test code!

--- app/src/main/java/com/example/guessthenumber/randomNumber/randomNumber.kt
+++ app/src/main/java/com/example/guessthenumber/randomNumber/randomNumber.kt
@@ -4,7 +4,7 @@ class randomNumber { //Take from 1 to 100 range and pick randomly one with the C
     val potentialNumbers = 1..10
     var solution = 0
     fun createGuess(){
-        solution = 5 // For testing only
-        //solution = potentialNumbers.random()
+//        solution = 5 // For testing only
+        solution = potentialNumbers.random()
     }
 }
